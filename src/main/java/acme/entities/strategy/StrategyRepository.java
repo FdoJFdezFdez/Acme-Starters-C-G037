40b110da93ae4564ad6ccf03dc53528cb501cf81
LMX8564 <eleferrei@alum.us.es>
2026-02-20 18:57:39 +0100

    Repository fixed

--- src/main/java/acme/entities/strategy/StrategyRepository.java
+++ src/main/java/acme/entities/strategy/StrategyRepository.java
@@ -2,6 +2,7 @@
 package acme.entities.strategy;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
@@ -10,5 +11,5 @@ import acme.client.repositories.AbstractRepository;
 public interface StrategyRepository extends AbstractRepository {
 
 	@Query("SELECT SUM(t.expectedPercentage) FROM Tactic t WHERE t.strategy.id = :id")
-	Double calculateExpectedPercentage(int strategyId);
+	Double calculateExpectedPercentage(@Param("id") int strategyId);
 }
