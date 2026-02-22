5c67c0d49a70fcb947c79531b11a4df1a42f3c4a
Angelo Sho Moraschi <68395930+amoraschi@users.noreply.github.com>
2026-02-22 17:37:23 +0100

    Finish implementing constraints

--- src/main/java/acme/entities/sponsors/Donation.java
+++ src/main/java/acme/entities/sponsors/Donation.java
@@ -10,10 +10,11 @@ import acme.client.components.basis.AbstractEntity;
 import acme.client.components.datatypes.Money;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidMoney;
+import acme.constraints.ValidHeader;
+import acme.constraints.ValidText;
 import lombok.Getter;
 import lombok.Setter;
 
-// TODO: Implement @Valid
 @Entity
 @Getter
 @Setter
@@ -22,12 +23,12 @@ public class Donation extends AbstractEntity {
 	private static final long	serialVersionUID	= 1L;
 
 	@Mandatory
-	// @ValidHeader
+	@ValidHeader
 	@Column
 	private String				name;
 
 	@Mandatory
-	// @ValidText
+	@ValidText
 	@Column
 	private String				notes;
 
